b3043dd73ed72e313996b3f648a96954530db61b
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-09-25 11:05:21 -0700

    clean API class and add docstrings

--- src/yc_central/api/data_api.py
+++ src/yc_central/api/data_api.py
@@ -7,18 +7,28 @@ import aiohttp
 
 class AsyncDataAPI:
     """
-    API class to fetch yield curve data async from AlphaVantage API.
-
-    :param api_key: AlphaVantage API key available at https://www.alphavantage.co/support/#api-key
-    :param interval: Interval for data being retrieved (i.e. daily, weekly, or monthly).
+    Wrapper around AlphaVantageAPI to fetch historical treasury data.
     """
 
-    def __init__(self, api_key, interval):
+    def __init__(self, api_key):
         self.base_url = "https://www.alphavantage.co/query?"
         self.api_key = api_key
-        self.interval = interval
+        self.yc_data = None
 
     async def get_data(self, endpoint, params=None):
+        """
+        Fetches data from the specified endpoint of the AlphaVantage API.
+
+        Args:
+            endpoint (str): The specific API endpoint to fetch data from.
+            params (dict, optional): Additional parameters to include in the request.
+
+        Returns:
+            dict: The JSON response from the API if the request is successful.
+
+        Raises:
+            aiohttp.ClientResponseError: If the response status is not 200.
+        """
         headers = {
             "Authorization": f"Bearer {self.api_key}",
             "Content-Type": "application/json",
@@ -28,27 +38,40 @@ class AsyncDataAPI:
             async with session.get(url, headers=headers, params=params) as response:
                 if response.status == 200:
                     return await response.json()
-                response.raise_for_status()
+                else:
+                    response.raise_for_status()
 
-    async def get_yields(self, interval: str = "daily") -> dict:
+    async def get_yields(self, interval: str = "daily"):
+        """
+        Fetches yield data for various yield maturities from the AlphaVantage API.
 
+        Args:
+            interval (str): The interval for the yield data. Must be one of 'daily', 'weekly', or 'monthly'.
+
+        Raises:
+            ValueError: If the provided interval is not one of the allowed values.
+
+        Returns:
+            None: The method populates the instance variable `yc_data` with the fetched yield data.
+        """
         if interval not in {"daily", "weekly", "monthly"}:
             raise ValueError("Interval value must be one of: 'daily', 'weekly', 'monthly'")
 
+        # dict of endpoints we will hit for historical data
         endpoints = {
-            "FedFunds": f"function=FEDERAL_FUNDS_RATE&interval={self.interval}&apikey={self.api_key}",
-            "3month": f"function=TREASURY_YIELD&interval={self.interval}&maturity=3month&apikey={self.api_key}",
-            "2year": f"function=TREASURY_YIELD&interval={self.interval}&maturity=2year&apikey={self.api_key}",
-            "5year": f"function=TREASURY_YIELD&interval={self.interval}&maturity=5year&apikey={self.api_key}",
-            "7year": f"function=TREASURY_YIELD&interval={self.interval}&maturity=7year&apikey={self.api_key}",
-            "10year": f"function=TREASURY_YIELD&interval={self.interval}&maturity=10year&apikey={self.api_key}",
-            "30year": f"function=TREASURY_YIELD&interval={self.interval}&maturity=30year&apikey={self.api_key}",
+            "FedFunds": f"function=FEDERAL_FUNDS_RATE&interval={interval}&apikey={self.api_key}",
+            "3month": f"function=TREASURY_YIELD&interval={interval}&maturity=3month&apikey={self.api_key}",
+            "2year": f"function=TREASURY_YIELD&interval={interval}&maturity=2year&apikey={self.api_key}",
+            "5year": f"function=TREASURY_YIELD&interval={interval}&maturity=5year&apikey={self.api_key}",
+            "7year": f"function=TREASURY_YIELD&interval={interval}&maturity=7year&apikey={self.api_key}",
+            "10year": f"function=TREASURY_YIELD&interval={interval}&maturity=10year&apikey={self.api_key}",
+            "30year": f"function=TREASURY_YIELD&interval={interval}&maturity=30year&apikey={self.api_key}",
         }
 
+        # retrieve data from API and save to class attribute
         tasks = [self.get_data(endpoint) for endpoint in endpoints.values()]
         responses = await asyncio.gather(*tasks)
-
-        return {
+        self.yc_data = {
             "FedFunds": responses[0],
             "3month": responses[1],
             "2year": responses[2],
