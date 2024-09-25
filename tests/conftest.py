6943bf0cb67671a037a39134db571ffa26247c60
Walker Hughes <74113220+walkerhughes@users.noreply.github.com>
2024-09-25 12:50:09 -0700

    add black reformatting

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ Register pytest plugins, fixtures, and hooks to be used during test execution.
 Docs: https://stackoverflow.com/questions/34466027/in-pytest-what-is-the-use-of-conftest-py-files
 """
 
-
 import sys
 from pathlib import Path
 
